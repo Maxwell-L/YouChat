943e51be2bd0ee686dcb1f153aae625cf1665202
Maxwell-L <974230257@qq.com>
2020-11-09 21:12:08 +0800

    Add SendMessageActivity

--- app/src/main/java/com/maxwell/youchat/ui/home/HomeFragment.java
+++ app/src/main/java/com/maxwell/youchat/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.maxwell.youchat.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,25 +14,24 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.maxwell.youchat.R;
+import com.maxwell.youchat.activity.SendMessageActivity;
 
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
-    private TextView textView;
+    private Button button;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                textView.setText(s);
-            }
+        button = root.findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), SendMessageActivity.class);
+            startActivity(intent);
         });
         return root;
     }
