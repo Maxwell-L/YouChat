b8c92d5d3a9a258ca50f46f08c639c2a8a140c79
Maxwell-L <974230257@qq.com>
2020-11-10 09:30:33 +0800

    Add EditText in SendMessageActivity

--- app/src/main/java/com/maxwell/youchat/ui/home/HomeFragment.java
+++ app/src/main/java/com/maxwell/youchat/ui/home/HomeFragment.java
@@ -21,14 +21,12 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
-    private Button button;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        button = root.findViewById(R.id.button);
+        final Button button = root.findViewById(R.id.button);
         button.setOnClickListener(v -> {
             Intent intent = new Intent(getActivity(), SendMessageActivity.class);
             startActivity(intent);
