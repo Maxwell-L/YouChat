d2c8532b22d13e1b46b2609828e425f0d9413502
Maxwell-L <974230257@qq.com>
2020-11-15 21:33:02 +0800

    make MessageDao

---
+++ app/src/main/java/com/maxwell/youchat/entity/Message.java
@@ -0,0 +1,82 @@
+package com.maxwell.youchat.entity;
+
+import android.support.v4.os.IResultReceiver;
+
+import androidx.annotation.NonNull;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+
+import org.greenrobot.greendao.annotation.Generated;
+
+@Entity
+public class Message {
+    @Id
+    private Long id;
+
+    @NonNull
+    private Long sendUserId;
+
+    @NonNull
+    private Long receiveUserId;
+
+    @NonNull
+    private Long createTime;
+
+    @NonNull
+    private String content;
+
+    @Generated(hash = 1234143995)
+    public Message(Long id, @NonNull Long sendUserId, @NonNull Long receiveUserId,
+            @NonNull Long createTime, @NonNull String content) {
+        this.id = id;
+        this.sendUserId = sendUserId;
+        this.receiveUserId = receiveUserId;
+        this.createTime = createTime;
+        this.content = content;
+    }
+
+    @Generated(hash = 637306882)
+    public Message() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getSendUserId() {
+        return sendUserId;
+    }
+
+    public Long getReceiveUserId() {
+        return receiveUserId;
+    }
+
+    public Long getCreateTime() {
+        return createTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setSendUserId(Long sendUserId) {
+        this.sendUserId = sendUserId;
+    }
+
+    public void setReceiveUserId(Long receiveUserId) {
+        this.receiveUserId = receiveUserId;
+    }
+
+    public void setCreateTime(Long createTime) {
+        this.createTime = createTime;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
