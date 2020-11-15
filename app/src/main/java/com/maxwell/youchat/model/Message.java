4cff95924b327bc0e7a68d28249aa8d0d9c50f7d
Maxwell-L <974230257@qq.com>
2020-11-15 19:23:16 +0800

    Add dependency:greenDAO

--- app/src/main/java/com/maxwell/youchat/model/Message.java
+++ app/src/main/java/com/maxwell/youchat/model/Message.java
@@ -3,28 +3,28 @@ package com.maxwell.youchat.model;
 import java.sql.Timestamp;
 
 public class Message {
-    private Integer id;
-    private Integer sendUserId;
-    private Integer receiveUserId;
+    private Long id;
+    private Long sendUserId;
+    private Long receiveUserId;
     private Timestamp createTime;
     private String content;
 
-    public Message(Integer sendUserId, Integer receiveUserId, Timestamp createTime, String content) {
+    public Message(Long sendUserId, Long receiveUserId, Timestamp createTime, String content) {
         this.sendUserId = sendUserId;
         this.receiveUserId = receiveUserId;
         this.createTime = createTime;
         this.content = content;
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public Integer getSendUserId() {
+    public Long getSendUserId() {
         return sendUserId;
     }
 
-    public Integer getReceiveUserId() {
+    public Long getReceiveUserId() {
         return receiveUserId;
     }
 
--- app/src/main/java/com/maxwell/youchat/ui/home/HomeViewModel.java
+++ app/src/main/java/com/maxwell/youchat/ui/home/HomeViewModel.java
@@ -4,9 +4,14 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mutableLiveData;
+
+    private MutableLiveData<List<HashMap<String, Object>>> itemList;
 
     public HomeViewModel() {
         mutableLiveData = new MutableLiveData<>();
